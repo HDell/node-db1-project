752e6302b58287fd555ebf2ea873881f1cc77ec7
Handell Desulme <handelldesulme@Handells-MacBook-Pro.local>
2020-02-17 12:52:43 -0500

    completes mvp w/ 1st two stretch

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const db = require('./data/dbConfig.js');
 
+const myRoute = require('./router.js');
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/', myRoute);
 
 module.exports = server;
